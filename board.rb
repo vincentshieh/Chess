e15435f0f5a00985b59ac9af1bf270befb6999fb
Vincent Shieh <vincent.shieh@gmail.com>
2015-02-24 18:09:15 -0800

    complete phase I and phase II of chess

---
+++ board.rb
@@ -0,0 +1,105 @@
+require_relative "./piece.rb"
+
+class Board
+
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(8) { Array.new(8) }
+    set_higher_pieces
+    set_pawns
+  end
+
+  def set_higher_pieces
+    @grid.each_index do |row_idx|
+      next if row_idx.between?(1, 6)
+
+      @grid.each_index do |col_idx|
+        pos = [row_idx, col_idx]
+
+        @grid[row_idx][col_idx] = case col_idx
+        when 0, 7
+          Rook.new(pos, self, row_color(row_idx))
+        when 1, 6
+          Knight.new(pos, self, row_color(row_idx))
+        when 2, 5
+          Bishop.new(pos, self, row_color(row_idx))
+        when 3
+          Queen.new(pos, self, row_color(row_idx))
+        when 4
+          King.new(pos, self, row_color(row_idx))
+        end
+      end
+    end
+  end
+
+  def set_pawns
+    @grid.each_index do |row_idx|
+      next unless row_idx == 1 || row_idx == 6
+      @grid.each_index do |col_idx|
+        pos = [row_idx, col_idx]
+
+        @grid[row_idx][col_idx] = Pawn.new(pos, self, row_color(row_idx))
+      end
+    end
+  end
+
+  def row_color(row_idx)
+    return :white if row_idx == 0 || row_idx == 1
+    return :black if row_idx == 6 || row_idx == 7
+
+    nil
+  end
+
+  def render
+    @grid.map do |row|
+      row.map do |piece|
+        piece.inspect
+      end.join("")
+    end.join("\n")
+  end
+
+  def [](position)
+    x, y = position
+    @grid[x][y]
+  end
+
+  def []=(position, piece)
+    x, y = position
+    @grid[x][y] = piece
+  end
+
+  def move(start, end_pos)
+    raise ArgumentError.new("No piece at #{start}.") if self[start].nil?
+    raise ArgumentError.new("Invalid end position.") if !self[start].moves.include?(end_pos)
+
+    self[end_pos] = self[start]
+    self[start] = nil
+
+    self[end_pos].pos = end_pos
+  end
+
+  def get_pieces(color)
+    pieces = []
+
+    @grid.each do |row|
+      row.each do |piece|
+        next if piece.nil?
+        pieces << piece if piece.color == color
+      end
+    end
+    pieces
+  end
+
+  def in_check?(color)
+    opposing_color = :white if color == :black
+    opposing_color = :black if color == :white
+    opposing_pieces = get_pieces(opposing_color)
+
+    opposing_pieces.any? { |opp_piece| opp_piece.moves.include?(get_king_pos(color))}
+  end
+
+  def get_king_pos(color)
+    get_pieces(color).select { |piece| piece.is_a?(King) }.first.pos
+  end
+end
---
+++ piece.rb
@@ -0,0 +1,175 @@
+require_relative "./board.rb"
+require "byebug"
+
+class Piece
+  attr_accessor :pos, :board
+  attr_reader :color
+
+  def initialize(pos, board, color)
+    @pos = pos
+    @board = board
+    @color = color
+  end
+
+  def moves
+  end
+
+  def blocked?
+  end
+
+  def inspect
+    { :pos => pos, :color => color, :class => self.class }.inspect
+  end
+
+  def edge_of_board?(pos)
+    pos.any? { |x| !x.between?(0, 7) }
+  end
+
+  def blocked?(pos)
+    !board[pos].nil?
+  end
+end
+
+class SlidingPiece < Piece
+
+  DELTAS = [[1,0], [-1, 0], [0, -1], [0, 1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+
+
+  def moves
+    all_moves = []
+
+    move_dirs.each do |dir|
+      all_moves += valid_moves_in_dir(dir)
+    end
+
+    all_moves
+  end
+
+  def valid_moves_in_dir(delta)
+    multiplier = 1
+    valid_moves = []
+    current_move = [pos[0] + multiplier * delta[0], pos[1] + multiplier * delta[1]]
+
+    until edge_of_board?(current_move) || (blocked?(current_move) &&
+                                           board[current_move].color == color)
+      valid_moves << current_move
+      break if blocked?(current_move)
+
+      multiplier += 1
+      current_move = [pos[0] + multiplier * delta[0], pos[1] + multiplier * delta[1]]
+    end
+
+    valid_moves
+  end
+
+
+end
+
+class Bishop < SlidingPiece
+
+  def move_dirs
+    DELTAS.drop(4)
+  end
+end
+
+class Rook < SlidingPiece
+
+  def move_dirs
+    DELTAS.take(4)
+  end
+end
+
+class Queen < SlidingPiece
+
+  def move_dirs
+    DELTAS
+  end
+end
+
+class SteppingPiece < Piece
+
+  KING_DELTAS = [[1, 0], [-1, 0], [0, -1], [0, 1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+  KNIGHT_DELTAS = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]]
+
+  def moves
+    valid_moves = []
+
+    move_dirs.each do |delta|
+      current_move = [pos[0] + delta[0], pos[1] + delta[1]]
+      unless edge_of_board?(current_move) || (blocked?(current_move) &&
+                                          board[current_move].color == color)
+        valid_moves << current_move
+      end
+    end
+
+   valid_moves
+  end
+end
+
+class Knight < SteppingPiece
+  def move_dirs
+    KNIGHT_DELTAS
+  end
+end
+
+class King < SteppingPiece
+  def move_dirs
+    KING_DELTAS
+  end
+end
+
+class Pawn < Piece
+  WHITE_DELTAS = [[1, 0], [2, 0], [1, 1], [1, -1]]
+  BLACK_DELTAS = [[-1, 0], [-2, 0], [-1, 1], [-1, -1]]
+
+  def moves
+    capture_moves + straight_moves
+  end
+
+  def capture_moves
+    valid_moves = []
+    deltas = move_dirs.drop(2)
+
+    deltas.each do |delta|
+      current_move = [pos[0] + delta[0], pos[1] + delta[1]]
+      current_piece = board[current_move]
+
+      unless edge_of_board?(current_move) || (blocked?(current_move) &&
+                                              current_piece.color == color)
+
+        valid_moves << current_move if blocked?(current_move) &&
+                                       current_piece.color != color
+      end
+    end
+
+    valid_moves
+  end
+
+  def straight_moves
+    valid_moves = []
+    deltas = move_dirs.take(2)
+    deltas = deltas.drop(1) unless (color == :white && pos[0] == 1) ||
+                                   (color == :black && pos[0] == 6)
+
+    one_step = [pos[0] + deltas[0][0], pos[1] + deltas[0][1]]
+
+    if board[one_step].nil? && !edge_of_board?(one_step)
+      valid_moves << one_step
+    end
+
+    if deltas.length == 2
+      two_step = [pos[0] + deltas[1][0], pos[1] + deltas[1][1]]
+
+      if board[two_step].nil? && board[one_step].nil? && !edge_of_board?(two_step)
+        valid_moves << two_step
+      end
+    end
+
+    valid_moves
+  end
+
+  def move_dirs
+    return WHITE_DELTAS if color == :white
+    BLACK_DELTAS
+  end
+end
